0cfba0a5dc476ae59b920ed233c96dde43af625b
AndreyZhdanov <b.r.v.k.su@gmail.com>
2015-10-10 18:15:08 +0800

    Code and Image file were added to repo

---
+++ plot1.R
@@ -0,0 +1,24 @@
+#R
+namePNG = 'plot1.png'
+sizePNGx = 480
+sizePNGy = 480
+strStartDate <- '01/02/2007'
+strEndDate <- '02/02/2007'
+nRows = -1
+
+cClasses <- c("character", "character")
+for (i in seq(7))
+	cClasses <- c(cClasses, "numeric")
+
+hpc <- read.csv2("household_power_consumption.txt", na.strings = '?', colClasses=cClasses, dec='.')
+hpc$posixDate <- as.Date(strptime(hpc$Date, "%d/%m/%Y"))
+posixStartDate <- as.Date(strptime(strStartDate, "%d/%m/%Y"))
+posixEndDate <- as.Date(strptime(strEndDate, "%d/%m/%Y"))
+hpcFiltred <- subset(hpc, posixDate >= posixStartDate & posixDate <= posixEndDate)
+rm(hpc)
+tHpc <- hpcFiltred[,3:9]
+tHpc$datetime <- strptime(paste(hpcFiltred$Date, hpcFiltred$Time), "%d/%m/%Y %H:%M:%S")
+rm(hpcFiltred)
+png(namePNG, width=sizePNGx, height=sizePNGy)
+hist(tHpc$Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,29 @@
+#R
+namePNG = 'plot3.png'
+sizePNGx = 480
+sizePNGy = 480
+strStartDate <- '01/02/2007'
+strEndDate <- '02/02/2007'
+nRows = -1
+
+cClasses <- c("character", "character")
+for (i in seq(7))
+	cClasses <- c(cClasses, "numeric")
+
+hpc <- read.csv2("household_power_consumption.txt", na.strings = '?', colClasses=cClasses, dec='.')
+hpc$posixDate <- as.Date(strptime(hpc$Date, "%d/%m/%Y"))
+posixStartDate <- as.Date(strptime(strStartDate, "%d/%m/%Y"))
+posixEndDate <- as.Date(strptime(strEndDate, "%d/%m/%Y"))
+hpcFiltred <- subset(hpc, posixDate >= posixStartDate & posixDate <= posixEndDate)
+rm(hpc)
+tHpc <- hpcFiltred[,3:9]
+tHpc$datetime <- strptime(paste(hpcFiltred$Date, hpcFiltred$Time), "%d/%m/%Y %H:%M:%S")
+rm(hpcFiltred)
+png(namePNG, width=sizePNGx, height=sizePNGy)
+with(tHpc, {
+	plot(datetime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+	points(datetime, Sub_metering_2, type="l", col="red")
+	points(datetime, Sub_metering_3, type="l", col="blue")
+})
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=paste0("Sub_metring_", c("1", "2", "3")), lwd=2)
+dev.off()
